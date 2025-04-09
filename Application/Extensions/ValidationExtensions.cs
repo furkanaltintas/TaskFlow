71f5d6cddd81d993b79706f7d1b1f7172193cd4f
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-10 02:55:09 +0300

    Validasyon yapısı v1

---
+++ Application/Extensions/ValidationExtensions.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation.Results;
+
+namespace Application.Extensions;
+
+public static class ValidationExtensions
+{
+    public static List<ValidationError> ToMap(this IEnumerable<ValidationFailure> errors)
+    {
+        List<ValidationError> result = new();
+
+        foreach (var error in errors)
+            result.Add(new(error.PropertyName, error.ErrorMessage));
+
+        return result;
+    }
+}
+
+public class ValidationError
+{
+    public ValidationError()
+    {
+        PropertyName = string.Empty;
+        ErrorMessage = string.Empty;
+    }
+
+    public ValidationError(string propertyName, string errorMessage)
+    {
+        PropertyName = propertyName;
+        ErrorMessage = errorMessage;
+    }
+
+    public string PropertyName { get; set; }
+    public string ErrorMessage { get; set; }
+}
