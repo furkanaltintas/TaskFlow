59d412eb89da3111f0e3e4a15286c9df02d09e0b
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-10 02:53:53 +0300

    persistence v1

---
+++ Persistence/ServiceRegistration.cs
@@ -0,0 +1,36 @@
+﻿using Application.Interfaces.UnitOfWorks;
+using Domain.Entitites;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Contexts;
+using Persistence.UnitOfWorks;
+
+namespace Persistence;
+
+public static class ServiceRegistration
+{
+    public static void AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(opt =>
+        {
+            opt.UseSqlServer(configuration.GetConnectionString("Local"));
+        });
+
+
+        services.AddIdentity<AppUser, AppRole>(options =>
+        {
+            options.Password.RequireDigit = false;
+            options.Password.RequiredLength = 3;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireLowercase = false;
+        })
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+    }
+}
